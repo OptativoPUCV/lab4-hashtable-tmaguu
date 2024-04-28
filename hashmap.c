8fe3a9e6106601bffa25f766f7e53f04071806ec
tmaguu <163349915+tmaguu@users.noreply.github.com>
2024-04-28 03:19:10 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -16,6 +16,29 @@ struct HashMap {
     long current; //indice del ultimo dato accedido
 };
 
+Pair * createPair( char * key,  void * value) {
+    Pair * new = (Pair *)malloc(sizeof(Pair));
+    new->key = key;
+    new->value = value;
+    return new;
+}
+
+long hash( char * key, long capacity) {
+    unsigned long hash = 0;
+     char * ptr;
+    for (ptr = key; *ptr != '\0'; ptr++) {
+        hash += hash*32 + tolower(*ptr);
+    }
+    return hash%capacity;
+}
+
+int is_equal(void* key1, void* key2){
+    if(key1==NULL || key2==NULL) return 0;
+    if(strcmp((char*)key1,(char*)key2) == 0) return 1;
+    return 0;
+}
+
+
 void insertMap(HashMap * map, char * key, void * value) 
 {
     if(map==NULL || key==NULL || value==NULL) return;
